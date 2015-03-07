7a5ab1f86cd84776f6c9dcaa45f26e66de549d13
Petr Šebek <petrsebek1@gmail.com>
2015-03-07 18:18:18 +0100

    Show table

--- formula-2-bdd.hs
+++ formula-2-bdd.hs
@@ -19,17 +19,30 @@ getFormula file = do
     let formula = map (splitOn ',') (lines contents)
     return formula
 
-variables :: Formula -> [String]
-variables formula = nub $ map (dropWhile (== '-')) $ concat formula
+getVariables :: Formula -> [String]
+getVariables formula = nub $ map (dropWhile (== '-')) $ concat formula
 
-{-getTruthTable :: [[String]] -> String-}
-{-getTruthTable formula = show getVariables formula-}
+showTable :: [[Int]] -> String
+showTable table = unlines [ unwords [ show x | x <- xs] | xs <- table ]
+
+isSatisfied :: [String] -> [Int] -> [String] -> Bool
+isSatisfied variables values clause = all (`elem` valuesClause) clause
+    where valuesClause = map (\ x -> if fst x == 0 then "-" ++ snd x else snd x) (zip values variables)
+
+boolToInt :: Bool -> Int
+boolToInt bool = if bool then 1 else 0
+
+getTruthTable :: Formula -> String
+getTruthTable formula =
+    let variables = getVariables formula
+        truthValues = sequence $ replicate (length variables) [0, 1]
+    in unwords variables ++ "\n" ++ showTable [values ++ [(boolToInt $ any (isSatisfied variables values) formula)] | values <-truthValues ]
 
 processCommand :: String -> Formula -> String
 processCommand command formula
     | command == "-i" = show formula
-    | command == "-v" = show $ variables formula
-    {-| command == "-t" = getTruthTable formula-}
+    | command == "-v" = show $ getVariables formula
+    | command == "-t" = getTruthTable formula
     | otherwise = "Unknown command: " ++ command
 
 main = do
