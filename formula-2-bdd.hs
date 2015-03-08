7a6451f5151b1a9f3f42f0ef9421e5dc0bbcba02
Petr Šebek <petrsebek1@gmail.com>
2015-03-08 21:11:33 +0100

    Formating and deleting unnecessary functions

--- formula-2-bdd.hs
+++ formula-2-bdd.hs
@@ -17,6 +17,7 @@ splitWhen p s = case dropWhile p s of
 splitOn :: Char -> String -> [String]
 splitOn d = splitWhen ( == d)
 
+--------------------------------------------------------------------------------
 -- | type declaration for Formula
 type Formula = [[String]]
 
@@ -36,18 +37,12 @@ isSatisfied :: [String] -> [Bool] -> [String] -> Bool
 isSatisfied variables values clause = all (`elem` valuesClause) clause
     where valuesClause = map (\ x -> if fst x == False then "-" ++ snd x else snd x) (zip values variables)
 
--- | convert True to 1, False to 0
-boolToInt :: Bool -> Int
-boolToInt bool = if bool then 1 else 0
-
-intToBool :: Int -> Bool
-intToBool int = if int == 0 then False else True
-
+--------------------------------------------------------------------------------
 type TruthTable = [[Bool]]
 
 -- | retun String representing table
 showTable :: TruthTable-> String
-showTable table = unlines [ unwords [ show $ boolToInt x | x <- xs] | xs <- table ]
+showTable table = unlines [ unwords [ show $ fromEnum x | x <- xs] | xs <- table ]
 
 -- | creates truth table according to formula
 getTruthTable :: Formula -> TruthTable
@@ -56,35 +51,10 @@ getTruthTable formula =
         truthValues = sequence $ replicate (length variables) [False, True]
     in [values ++ [ any (isSatisfied variables values) formula] | values <- truthValues ]
 
--- | check formula
-check :: Formula -> Bool
-check formula = equivalent bdd tt variables && equivalent rbdd tt variables
-    where bdd = getBDD formula
-          rbdd = reduceBDD bdd
-          tt = getTruthTable formula
-          variables = getVariables formula
 
--- | runs command on formula
-processCommand :: String -> Formula -> String
-processCommand command formula
-    | command == "-i" = (show formula) ++ "\n"
-    | command == "-v" = (show $ getVariables formula) ++ "\n"
-    | command == "-b" = showBDD $ getBDD formula
-    | command == "-t" = unwords (getVariables formula) ++ "\n" ++ (showTable $ getTruthTable formula)
-    | command == "-r" = showBDD $  reduceBDD $ getBDD formula
-    | command == "-c" = (show $ check formula) ++ "\n"
-    | otherwise = "Unknown command: " ++ command
-
-
+--------------------------------------------------------------------------------
 -- | data structure for Binary decision diagram
 data BDD a = Leaf Bool | Node a (BDD a) (BDD a) deriving (Show, Eq)
-
--- | Reduce Ordered BDD to ROBDD
-reduceBDD :: (Eq a) => BDD a -> BDD a
-reduceBDD (Node x l r)
-    | l == r = reduceBDD l
-    | l /= r = (Node x (reduceBDD l) (reduceBDD r))
-reduceBDD self@(Leaf b) = self
 
 -- | Set leaf under path [Bool] of bdd to new_value
 setLeaf :: [Bool] -> BDD a -> Bool -> BDD a
@@ -104,6 +74,22 @@ getLeaf (_:_) (Leaf b) = b
 getLeaf [] (Leaf b) = b
 getLeaf [] (Node _ _ _) = error "You are not in a leaf!"
 
+-- | getValue from Node or Leaf
+getValue :: BDD String -> String
+getValue (Node x _ _) = x
+getValue (Leaf b) = show $ fromEnum b
+
+-- | return representation of BDD, if empty return only bool value
+showBDD :: BDD String -> String
+showBDD (Node x l r) = showBDD' (Node x l r)
+showBDD (Leaf b) = showBDD'' (Leaf b)
+
+showBDD' (Node x l r) = x ++ "->" ++ (getValue l) ++ "\n" ++ x ++ "=>" ++ (getValue r) ++ "\n" ++ (showBDD' l) ++ (showBDD' r)
+showBDD' (Leaf b) = ""
+
+showBDD'' (Leaf b) = (show $ fromEnum b) ++ "\n"
+
+-- | Tests whether BDD is equivalent to truthtables in order of variables
 equivalent :: BDD String -> TruthTable -> [String] -> Bool
 equivalent  bdd tt variables = all (==True) [(last x) == (getLeaf (zip variables (init x)) bdd) | x <-tt]
 
@@ -124,20 +110,32 @@ getBDD formula =
         truthTable = getTruthTable formula
     in evaluateBDD bdd truthTable
 
--- | getValue from Node or Leaf
-getValue :: BDD String -> String
-getValue (Node x _ _) = x
-getValue (Leaf b) = show $ boolToInt b
+-- | Reduce Ordered BDD to ROBDD
+reduceBDD :: (Eq a) => BDD a -> BDD a
+reduceBDD (Node x l r)
+    | l == r = reduceBDD l
+    | l /= r = (Node x (reduceBDD l) (reduceBDD r))
+reduceBDD self@(Leaf b) = self
 
--- | return representation of BDD, if empty return only bool value
-showBDD :: BDD String -> String
-showBDD (Node x l r) = showBDD' (Node x l r)
-showBDD (Leaf b) = showBDD'' (Leaf b)
+-- | check formula correspondence to BDD and ROBDD
+check :: Formula -> Bool
+check formula = equivalent bdd tt variables && equivalent rbdd tt variables
+    where bdd = getBDD formula
+          rbdd = reduceBDD bdd
+          tt = getTruthTable formula
+          variables = getVariables formula
 
-showBDD' (Node x l r) = x ++ "->" ++ (getValue l) ++ "\n" ++ x ++ "=>" ++ (getValue r) ++ "\n" ++ (showBDD' l) ++ (showBDD' r)
-showBDD' (Leaf b) = ""
-
-showBDD'' (Leaf b) = (show $ boolToInt b) ++ "\n"
+--------------------------------------------------------------------------------
+-- | runs command on formula
+processCommand :: String -> Formula -> String
+processCommand command formula
+    | command == "-i" = (show formula) ++ "\n"
+    | command == "-v" = (show $ getVariables formula) ++ "\n"
+    | command == "-b" = showBDD $ getBDD formula
+    | command == "-t" = unwords (getVariables formula) ++ "\n" ++ (showTable $ getTruthTable formula)
+    | command == "-r" = showBDD $  reduceBDD $ getBDD formula
+    | command == "-c" = (show $ check formula) ++ "\n"
+    | otherwise = "Unknown command: " ++ command
 
 main = do
     [command, file] <- getArgs
